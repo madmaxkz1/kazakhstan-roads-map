0dec25ceb00ef3e4ec207b2f825ec1c83ca2e6c7
madmaxkz1 <max_kz@inbox.ru>
2026-04-23 19:18:09 +0500

    Add files via upload

---
+++ border_crossings.js
@@ -0,0 +1,68 @@
+window.BORDER_CROSSINGS = [
+  {
+    name: "Жана жол",
+    roadRefs: ["KAZ02"],
+    country: "РФ",
+    coordinates: [69.07, 54.9]
+  },
+  {
+    name: "Кайрак",
+    roadRefs: ["KAZ03"],
+    country: "РФ",
+    coordinates: [61.16, 53.99]
+  },
+  {
+    name: "Косак",
+    roadRefs: ["KAZ04"],
+    country: "РФ",
+    coordinates: [76.26, 53.33]
+  },
+  {
+    name: "Жайсан",
+    roadRefs: ["KAZ06"],
+    country: "РФ",
+    coordinates: [56.1, 50.78]
+  },
+  {
+    name: "Нур Жолы",
+    roadRefs: ["KAZ06"],
+    country: "КНР",
+    coordinates: [80.39, 44.22]
+  },
+  {
+    name: "Урлютобе",
+    roadRefs: ["KAZ07"],
+    country: "РФ",
+    coordinates: [76.26, 53.33]
+  },
+  {
+    name: "Майкапшагай",
+    roadRefs: ["KAZ07"],
+    country: "КНР",
+    coordinates: [85.61, 47.52]
+  },
+  {
+    name: "Убе",
+    roadRefs: ["KAZ08"],
+    country: "РФ",
+    coordinates: [82.57, 50.76]
+  },
+  {
+    name: "Курмангазы",
+    roadRefs: ["KAZ09"],
+    country: "РФ",
+    coordinates: [48.75, 46.56]
+  },
+  {
+    name: "Сырым",
+    roadRefs: ["KAZ10"],
+    country: "РФ",
+    coordinates: [51.0, 51.66]
+  },
+  {
+    name: "Бидайык",
+    roadRefs: ["KAZ16"],
+    country: "РФ",
+    coordinates: [72.77, 54.12]
+  }
+];
